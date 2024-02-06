5e17bc16f659ca09defca45b4ae26c39b3bf4d56
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-02-07 02:02:26 +0700

    Add ProductTest class

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/ProductTest.java
@@ -0,0 +1,33 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest{
+    Product product;
+
+    @BeforeEach
+    void setUp(){
+        this.product = new Product();
+        this.product.setProductId("2b0c5fcf-1198-474c-bd1e-f0713131b326");
+        this.product.setProductName("Sata Andagi");
+        this.product.setProductQuantity(101);
+    }
+
+    @Test
+    void testGetProductId(){
+        assertEquals("2b0c5fcf-1198-474c-bd1e-f0713131b326", this.product.getProductId());
+    }
+
+    @Test
+    void testGetProductName(){
+        assertEquals("Sata Andagi", this.product.getProductName());
+    }
+
+    @Test
+    void testGetProductQuantity(){
+        assertEquals(101, this.product.getProductQuantity());
+    }
+}

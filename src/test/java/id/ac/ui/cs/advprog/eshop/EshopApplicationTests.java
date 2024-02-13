28616d5fe6f6f27519ae53d8977e297a1b27f792
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-02-13 18:26:52 +0700

    add unit tests for product service and app's main method

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,19 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+    @Autowired
+    private EshopApplication application;
+
     @Test
     void contextLoads() {
+        assertDoesNotThrow(() -> application.main(new String[] {}));
     }
 
 }

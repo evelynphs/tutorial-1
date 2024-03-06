e03f9766ae59dba8ce01ac62165b0f8fd6b1b3bf
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-03-06 10:48:12 +0700

    [RED] Add tests for Order model

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.eshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class EshopApplicationTests {
